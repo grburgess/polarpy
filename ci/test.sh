8d31dc6fbb7ac2120ccfccf2a8bd692875bd673d
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-08-05 11:42:29 +0200

    add

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+
+# Make matplotlib non-interactive (otherwise it will crash
+# all the tests)
+export MPLBACKEND='Agg'
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+cd ~
+rm -rf my_work_dir
+mkdir my_work_dir
+# Copy also dot files (.*)
+shopt -s dotglob
+cp -R ${TRAVIS_BUILD_DIR}/* my_work_dir/
+
+cd my_work_dir
+
+
 python -m pytest -vv --cov=polarpy
