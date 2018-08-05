a27ad0c2acab430cfb24152dd8e57e27f02ba476
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-08-05 17:59:16 +0200

    add more tests

--- polarpy/test/test_polarlike.py
+++ polarpy/test/test_polarlike.py
@@ -6,14 +6,14 @@ from polarpy.polarlike import PolarLike
 from polarpy.polar2hdf5 import polar_polarization_to_hdf5
 
 from threeML.io.file_utils import sanitize_filename
-data_path = sanitize_filename(os.environ.get('POLAR_TEST_DATA_DIR'),abspath=True)
+data_path = sanitize_filename(os.environ.get('POLAR_TEST_DATA_DIR'), abspath=True)
 
+from astromodels import LinearPolarization, Powerlaw, Model, PointSource, SpectralComponent
 
+root_file = os.path.join(data_path, 'fold_spec.root')
 
-root_file = os.path.join(data_path,'fold_spec.root')
-    
 outfile = 'testrsp.h5'
-    
+
 polar_polarization_to_hdf5(polarization_root_file=root_file, hdf5_out_file=outfile)
 
 # now make sure the polar response works
@@ -21,11 +21,31 @@ polar_polarization_to_hdf5(polarization_root_file=root_file, hdf5_out_file=outfi
 prsp = PolarResponse(outfile)
 
 
-
 def test_countructor():
 
-    obs = os.path.join(data_path,'test_mcf.h5')
-    bak = os.path.join(data_path,'test_mcf.h5')
-    
-    polarlike = PolarLike('test',observation=obs, background=bak,response=prsp, interval_number=1)
-    
+    obs = os.path.join(data_path, 'test_mcf.h5')
+    bak = os.path.join(data_path, 'test_mcf.h5')
+
+    polarlike = PolarLike('test', observation=obs, background=bak, response=prsp, interval_number=1)
+
+
+def test_setting_model():
+
+    obs = os.path.join(data_path, 'test_mcf.h5')
+    bak = os.path.join(data_path, 'test_mcf.h5')
+
+    polarlike = PolarLike('test', observation=obs, background=bak, response=prsp, interval_number=1)
+
+    pl = Powerlaw()
+    pz = LinearPolarization(10, 10)
+
+    sc = SpectralComponent(pl, pz)
+
+    ps = PointSource('test', 0, 0, spectral_components=[sc])
+    model = Model(ps)
+
+    polarlike.set_model(model)
+
+    polarlike.get_log_like()
+
+    sim = polarlike.get_simulated_dataset()
