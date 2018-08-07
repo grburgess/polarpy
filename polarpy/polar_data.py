87d6a94fbfb4d2114e184116dc9e7e651111f2d2
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-08-07 09:05:07 +0200

    added scattering angles

--- polarpy/polar_data.py
+++ polarpy/polar_data.py
@@ -6,7 +6,7 @@ import h5py
 
 
 class POLARData(object):
-    def __init__(self, polar_hdf5_file, reference_time=0.,):
+    def __init__(self, polar_hdf5_file, polar_hdf5_response, reference_time=0.):
         """
         container class that converts raw POLAR HDF5 data into useful python
         variables
@@ -17,6 +17,10 @@ class POLARData(object):
         :param rsp_file: path to rsp file
         """
 
+
+        
+
+        
         with h5py.File(polar_hdf5_file,'r') as f:
 
             rsp_grp = f['rsp']
@@ -25,9 +29,6 @@ class POLARData(object):
             ebounds = rsp_grp['ebounds'].value
             mc_low = rsp_grp['mc_low'].value 
             mc_high = rsp_grp['mc_high'].value
-
-
-
 
 
             # open the event file
@@ -57,6 +58,18 @@ class POLARData(object):
             # these bins are preliminary
 
 
+            # now do the scattering angles
+
+            scattering_angles = f['scatter_angle'].value
+
+            # clear the bad scattering angles
+            idx = scattering_angles != -1
+
+            self._scattering_angle_time = (f['time'].value)[idx] - reference_time
+            self._scattering_angle_dead_time_fraction = (f['dead_ratio'].value)[idx]
+            self._scattering_angles = scattering_angles[idx]
+            
+
         # build the POLAR response
 
         mc_energies = np.append(mc_low, mc_high[-1])
@@ -69,6 +82,18 @@ class POLARData(object):
 
         self._binned_pha = np.digitize(pha, ebounds)
 
+        # bin the scattering_angles
+
+        with h5py.File(polar_hdf5_response, 'r') as f:
+
+            scatter_bounds = f['bins'].value
+
+
+        self._scattering_bins = scatter_bounds
+        self._binned_scattering_angles = np.digitize(self._scattering_angles, scatter_bounds)
+        
+
+        
     @property
     def pha(self):
         return self._binned_pha
@@ -89,3 +114,22 @@ class POLARData(object):
     def n_channels(self):
 
         return len(self._rsp.ebounds) - 1
+
+    @property
+    def scattering_angles(self):
+
+        return self._binned_scattering_angles
+
+    @property
+    def scattering_angle_time(self):
+
+        return self._scattering_angle_time
+
+    @property
+    def scattering_angle_dead_time_fraction(self):
+        return self._scattering_angle_dead_time_fraction
+
+    @property
+    def n_scattering_bins(self):
+
+        return len(self._scattering_bins) - 1
