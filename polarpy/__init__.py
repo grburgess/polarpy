b7b6ef28bb12b51e03475f9312de8c91fa750252
grburgess <jmichaelburgess@gmail.com>
2020-05-01 17:56:55 +0200

    move to a more modern setup

--- polarpy/__init__.py
+++ polarpy/__init__.py
@@ -1,9 +1,11 @@
-from polarpy.polarlike import PolarLike
 from polarpy.polar_response import PolarResponse
+from polarpy.polarlike import PolarLike
+
+from ._version import get_versions
 
 try:
     import ROOT
-    
+
     has_root = True
 
 except ImportError:
@@ -12,3 +14,6 @@ except ImportError:
 
 if has_root:
     from polarpy.polar2hdf5 import polar_polarization_to_hdf5, polar_spectra_to_hdf5
+
+__version__ = get_versions()['version']
+del get_versions
